f2460faf75ad1c84c4c42c9489cb3e3652728d3e
Michael Snoyman <michael@snoyman.com>
2010-10-17 16:54:13 +0200

    Support for alternatives

--- Network/Mail/Mime.hs
+++ Network/Mail/Mime.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Yesod.Mail
+module Network.Mail.Mime
     ( -- * Datatypes
       Boundary (..)
     , Mail (..)
+    , Alternatives
     , Part (..)
     , Encoding (..)
-    , Disposition (..)
       -- * Render a message
     , renderMail
     , renderMail'
@@ -27,6 +27,7 @@ import System.IO
 import System.Exit
 import Codec.Binary.Base64 (encode)
 import Control.Monad ((<=<))
+import Data.List (intersperse)
 
 -- | Generates a random sequence of alphanumerics of the given length.
 randomString :: RandomGen d => Int -> d -> (String, d)
@@ -52,74 +53,132 @@ instance Random Boundary where
 -- | An entire mail message.
 data Mail = Mail
     { mailHeaders :: [(String, String)] -- ^ All headers, including to, from subject.
-    , mailPlain :: String -- ^ Content before the MIME message begins, only seen by very old clients.
-    , mailParts :: [Part]
+    -- | A list of different sets of alternatives. As a concrete example:
+    --
+    -- > mailParts = [ [textVersion, htmlVersion], [attachment1], [attachment1]]
+    --
+    -- Make sure when specifying alternatives to place the most preferred
+    -- version last.
+    , mailParts :: [Alternatives]
     }
 
 -- | How to encode a single part. You should use 'Base64' for binary data.
 data Encoding = None | Base64
 
--- | Basically, 'Inline' is for things to be shown in the mail client, 'Attachment' for files to be downloaded.
-data Disposition = Inline
-                 | Attachment String -- ^ the name of the file
+-- | Multiple alternative representations of the same data. For example, you
+-- could provide a plain-text and HTML version of a message.
+type Alternatives = [Part]
 
 -- | A single part of a multipart message.
 data Part = Part
     { partType :: String -- ^ content type
     , partEncoding :: Encoding
-    , partDisposition :: Disposition
+    -- | The filename for this part, if it is to be sent with an attachemnt
+    -- disposition.
+    , partFilename :: Maybe String
     , partContent :: L.ByteString
     }
 
--- | Render a 'Mail' with a given 'Boundary'.
---
--- You can also get a randomly-generated 'Boundary' by using renderMail'.
-renderMail :: Boundary -> Mail -> L.ByteString
-renderMail (Boundary b) (Mail headers plain parts) = toLazyByteString $ mconcat
-    [ mconcat $ map showHeader headers
-    , mconcat $ map showHeader
-        [ ("MIME-Version", "1.0")
-        , ("Content-Type", "multipart/mixed; boundary=\""
-            ++ b ++ "\"")
-        ]
-    , fromByteString "\n"
-    , fromString plain
-    , mconcat $ map showPart parts
-    , fromByteString "\n--"
-    , fromString b
-    , fromByteString "--"
-    ]
+type Headers = [(String, String)]
+type Pair = (Headers, Builder)
+
+partToPair :: Part -> Pair
+partToPair (Part contentType encoding disposition content) =
+    (headers, builder)
   where
-    showHeader (k, v) = mconcat
-        [ fromString k
-        , fromByteString ": "
-        , fromString v
-        , fromByteString "\n"
-        ]
-    showPart (Part contentType encoding disposition content) = mconcat
-        [ fromByteString "\n--"
-        , fromString b
-        , fromByteString "\n"
-        , showHeader ("Content-Type", contentType)
-        , case encoding of
-            None -> mempty
-            Base64 -> showHeader ("Content-Transfer-Encoding", "base64")
-        , case disposition of
-            Inline -> mempty
-            Attachment filename ->
-                showHeader ("Content-Disposition", "attachment; filename=" ++ filename)
-        , fromByteString "\n"
-        , case encoding of
+    headers =
+        ((:) ("Content-Type", contentType))
+      $ (case encoding of
+            None -> id
+            Base64 -> (:) ("Content-Transfer-Encoding", "base64"))
+      $ (case disposition of
+            Nothing -> id
+            Just fn ->
+                (:) ("Content-Disposition", "attachment; filename=" ++ fn))
+      $ []
+    builder =
+        case encoding of
             None -> writeList writeByteString $ L.toChunks content
             Base64 -> writeList writeByte $ map (toEnum . fromEnum)
                     $ encode $ L.unpack content
+
+showPairs :: RandomGen g
+          => String -- ^ multipart type, eg mixed, alternative
+          -> [Pair]
+          -> g
+          -> (Pair, g)
+showPairs _ [] _ = error "renderParts called with null parts"
+showPairs _ [pair] gen = (pair, gen)
+showPairs mtype parts gen =
+    ((headers, builder), gen')
+  where
+    (Boundary b, gen') = random gen
+    headers =
+        [ ("Content-Type", concat
+            [ "multipart/"
+            , mtype
+            , "; boundary=\""
+            , b
+            , "\""
+            ])
         ]
+    builder = mconcat
+        [ mconcat $ intersperse (fromByteString "\n")
+                  $ map (showBoundPart $ Boundary b) parts
+        , showBoundEnd $ Boundary b
+        ]
+
+-- | Render a 'Mail' with a given 'RandomGen' for producing boundaries.
+renderMail :: RandomGen g => g -> Mail -> (L.ByteString, g)
+renderMail g0 (Mail headers parts) =
+    (toLazyByteString builder, g'')
+  where
+    pairs = map (map partToPair) parts
+    (pairs', g') = helper g0 $ map (showPairs "alternative") pairs
+    helper :: g -> [g -> (x, g)] -> ([x], g)
+    helper g [] = ([], g)
+    helper g (x:xs) =
+        let (b, g') = x g
+            (bs, g'') = helper g' xs
+         in (b : bs, g'')
+    ((finalHeaders, finalBuilder), g'') = showPairs "mixed" pairs' g'
+    builder = mconcat
+        [ mconcat $ map showHeader headers
+        , showHeader ("MIME-Version", "1.0")
+        , mconcat $ map showHeader finalHeaders
+        , fromByteString "\n"
+        , finalBuilder
+        ]
+
+showHeader (k, v) = mconcat
+    [ fromString k
+    , fromByteString ": "
+    , fromString v
+    , fromByteString "\n"
+    ]
+
+showBoundPart (Boundary b) (headers, content) = mconcat
+    [ fromByteString "--"
+    , fromString b
+    , fromByteString "\n"
+    , mconcat $ map showHeader headers
+    , fromByteString "\n"
+    , content
+    ]
+
+showBoundEnd (Boundary b) = mconcat
+    [ fromByteString "\n--"
+    , fromString b
+    , fromByteString "--"
+    ]
 
 -- | Like 'renderMail', but generates a random boundary.
 renderMail' :: Mail -> IO L.ByteString
 renderMail' m = do
-    b <- randomIO
-    return $ renderMail b m
+    g <- getStdGen
+    let (lbs, g') = renderMail g m
+    setStdGen g'
+    return lbs
 
 -- | Send a fully-formed email message via the sendmail executable.
 sendmail :: L.ByteString -> IO ()
